c7303196f3d322d8a9486abdbb0480c5c9b6801e
satoshi-yamada <satoshi.yamada@tafs.co.jp>
2024-01-26 13:28:21 +0900

    コミット

--- src/test/java/com/example/githubactionssandboxpub/GithubActionsSandboxPubApplicationTests.java
+++ src/test/java/com/example/githubactionssandboxpub/GithubActionsSandboxPubApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.githubactionssandboxpub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GithubActionsSandboxPubApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat("aaa").isEqualTo("aaa");
 	}
 
 }
