d8ba4f8cefb3cd8a57023edddc46a11282930d81
satoshi-yamada <satoshi.yamada@tafs.co.jp>
2024-01-26 13:23:50 +0900

    first commit

---
+++ src/main/java/com/example/githubactionssandboxpub/GithubActionsSandboxPubApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionssandboxpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsSandboxPubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsSandboxPubApplication.class, args);
+	}
+
+}
