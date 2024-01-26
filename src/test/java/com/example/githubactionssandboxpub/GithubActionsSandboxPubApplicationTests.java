501adb6ea23ebcad75ac410f874671f28a5f1731
satoshi-yamada <satoshi.yamada@tafs.co.jp>
2024-01-26 13:28:56 +0900

    コミット

--- src/test/java/com/example/githubactionssandboxpub/GithubActionsSandboxPubApplicationTests.java
+++ src/test/java/com/example/githubactionssandboxpub/GithubActionsSandboxPubApplicationTests.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class GithubActionsSandboxPubApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void テストケース１() {
 		assertThat("aaa").isEqualTo("aaa");
 	}
 
